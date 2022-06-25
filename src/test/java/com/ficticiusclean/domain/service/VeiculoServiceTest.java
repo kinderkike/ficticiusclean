cd18caec17994cae5bbbde3f47a1da4a634ab25f
Francisco <kinderkike@gmail.com>
2022-06-25 11:19:00 -0300

    Criando teste de cadastro

--- src/test/java/com/ficticiusclean/domain/service/VeiculoServiceTest.java
+++ src/test/java/com/ficticiusclean/domain/service/VeiculoServiceTest.java
@@ -1,12 +1,33 @@
 package com.ficticiusclean.domain.service;
 
-import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.ficticiusclean.domain.model.Veiculo;
+import com.ficticiusclean.domain.repository.VeiculoRepository;
+
+@RunWith(MockitoJUnitRunner.class)
 public class VeiculoServiceTest {
+	
+	@Mock
+	private VeiculoRepository veiculoRepository;
+	
+	@InjectMocks
+	private VeiculoService veiculoService; 
 	
 	@Test
 	public void deveSalvarVeiculo() {
+		Veiculo veiculo = new Veiculo();
+		veiculo.setId(1l);
 		
+		veiculoService.cadastrar(veiculo);
+		
+		verify(veiculoRepository).save(veiculo);
 	}
 
 }
